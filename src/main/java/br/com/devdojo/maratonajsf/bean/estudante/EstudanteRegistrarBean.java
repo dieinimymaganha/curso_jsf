fb7c56efef6b8cac16577418c68becf8ca99b7c6
Dieinimy Maganha <dieinimy@gmail.com>
2019-10-03 21:20:48 -0300

    Incluindo exibição de array e list, e como usar um for

--- src/main/java/br/com/devdojo/maratonajsf/bean/estudante/EstudanteRegistrarBean.java
+++ src/main/java/br/com/devdojo/maratonajsf/bean/estudante/EstudanteRegistrarBean.java
@@ -4,11 +4,33 @@ import br.com.devdojo.maratonajsf.model.Estudante;
 
 import javax.inject.Named;
 import java.io.Serializable;
+import java.util.List;
+
+import static java.util.Arrays.asList;
 
 @Named
 public class EstudanteRegistrarBean implements Serializable {
     private Estudante estudante = new Estudante();
 
+
+    private String[] nomesArray = {"DevDojo", "eh", "foda"};
+    private List<String> nomesList = asList("William", "Wildnei", "Brenon", "Sandy");
+
+    public List<String> getNomesList() {
+        return nomesList;
+    }
+
+    public void setNomesList(List<String> nomesList) {
+        this.nomesList = nomesList;
+    }
+
+    public String[] getNomesArray() {
+        return nomesArray;
+    }
+
+    public void setNomesArray(String[] nomesArray) {
+        this.nomesArray = nomesArray;
+    }
 
     public Estudante getEstudante() {
         return estudante;
