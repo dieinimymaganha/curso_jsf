9e88c185a26a79d39d50b7a2243907258c505e0b
Dieinimy Maganha <dieinimy@gmail.com>
2019-10-13 11:57:03 -0300

    Iniciando a criação do flow utilizando classes java

---
+++ src/main/java/br/com/devdojo/maratonajsf/flowbuilder/NewRegistrationFlowBuilder.java
@@ -0,0 +1,24 @@
+package br.com.devdojo.maratonajsf.flowbuilder;
+
+import javax.enterprise.inject.Produces;
+import javax.faces.flow.Flow;
+import javax.faces.flow.builder.FlowBuilder;
+import javax.faces.flow.builder.FlowBuilderParameter;
+import javax.faces.flow.builder.FlowDefinition;
+import java.io.Serializable;
+
+public class NewRegistrationFlowBuilder implements Serializable {
+    @Produces
+    @FlowDefinition
+    public Flow defineFlow(@FlowBuilderParameter FlowBuilder flowBuilder){
+        String flowId = "newregistration";
+        flowBuilder.id("",flowId);
+        flowBuilder.viewNode(flowId, "/newregistration/newregistration.xhtml").markAsStartNode();
+        flowBuilder.viewNode(flowId, "/newregistration/newregistration2.xhtml");
+        flowBuilder.viewNode(flowId, "/newregistration/newregistration3.xhtml");
+
+        flowBuilder.returnNode("exitToInicio").fromOutcome("/inicioflow.xhtml");
+        flowBuilder.returnNode("exitToIndex").fromOutcome("/index.xhtml");
+        return flowBuilder.getFlow();
+    }
+}
